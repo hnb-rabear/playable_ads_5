f5e75e2cc9e99e9e266afdc63e8b43ef798cf8c2
RadBear <nbhung71711@gmail.com>
2024-03-13 16:56:02 +0700

    fix auto resize when screen rotate from vertical to horizontal

--- assets/Scripts/Core/AutoResizeUI.ts
+++ assets/Scripts/Core/AutoResizeUI.ts
@@ -7,14 +7,17 @@ export class AutoResizeUI extends Component {
     @property(Vec2) public standardRatio: Vec2 = new Vec2(1080, 1920);
 
     private m_standardSize: math.Size = new math.Size();
+    private m_preferWidthRatio: number = 0;
+
     protected start(): void {
         this.m_standardSize.set(this.node.getComponent(UITransform).contentSize);
+        // Fix the issue when screen is rotate from portrait to landscape
+        if (this.standardRatio.y > this.standardRatio.x) {
+            this.m_preferWidthRatio = this.standardRatio.x / this.standardRatio.y;
+        }
         window.addEventListener('resize', () => {
             this.resize();
         });
-    }
-
-    protected onEnable(): void {
         this.resize();
     }
 
@@ -22,6 +25,14 @@ export class AutoResizeUI extends Component {
         const curRatio = Math.round(window.innerWidth / window.innerHeight * 100) / 100;
         let standardRatio = Math.round(this.standardRatio.x / this.standardRatio.y * 100) / 100;
         let ratio = Math.round(standardRatio / curRatio * 100) / 100;
+
+        if (this.m_preferWidthRatio > 0 && curRatio > 1) {
+            const widthRatio = ratio * this.m_standardSize.x / window.innerWidth;
+            if (widthRatio < this.m_preferWidthRatio) {
+                ratio = this.m_preferWidthRatio;
+            }
+        }
+
         if (this.useScale)
             this.node.setScale(new Vec3(ratio, ratio, ratio));
         else {
